2be51ac6bdfbc57c9095882f5a66a6892aaed764
P Bridwell <patbridwell@cox.net>
2020-07-02 19:56:08 -0500

    alters get request and updates .env toude mongo connection at mlab

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import './customers.css';
+import axios from 'axios';
 
 class Customers extends Component {
-  constructor() {
-    super();
-    this.state = {
-      customers: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			customers: [],
+		};
+	}
 
-  componentDidMount() {
-    fetch('/api/customers')
-      .then(res => res.json())
-      .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
-  }
+	componentDidMount() {
+		axios
+			.get('/api/customers')
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+	}
 
-  render() {
-    return (
-      <div>
-        <h2>Customers</h2>
-        <ul>
-        {this.state.customers.map(customer => 
-          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
-        )}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h2>Customers</h2>
+				<ul>
+					{this.state.customers.map((customer) => (
+						<li key={customer.id}>
+							{customer.firstName} {customer.lastName}
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default Customers;
