36b0ed24cf8c476aa198afcccb8ac6e3054e03f2
P Bridwell <patbridwell@cox.net>
2020-07-07 17:46:12 -0500

    update client and server package.json to proxy 3001 to reflect server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require('./routes/api/users');
 const router = express.Router();
