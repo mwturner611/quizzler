709dd1ba937938ad3c4ec6328ffc13a88c58e12a
mwturner611 <mwturner611@gmail.com>
2020-07-13 18:14:17 -0500

    update to models and seed file. Removed old controller

--- models/Card.js
+++ models/Card.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const CardSchema = new Schema({
 	
     keyWord: { type: String, required: true },
-    definition: { type: String, required: true }
-    
+    definition: { type: String, required: true },
+    deckID: { type: Schema.Types.ObjectId, ref: 'Deck' }
 });
 
 module.exports = Card = mongoose.model('Card', CardSchema);
