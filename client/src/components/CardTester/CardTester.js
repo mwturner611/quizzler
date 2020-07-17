3756d679f0fcabb7a9615459ddfaee36c4c61f59
P Bridwell <patbridwell@cox.net>
2020-07-16 20:48:26 -0500

    adds transition group

--- client/src/components/CardTester/CardTester.js
+++ client/src/components/CardTester/CardTester.js
@@ -2,39 +2,39 @@ import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
 import './CardTester.css';
 
-
 function CardTester(props) {
+	const [isFlipped, setIsFlipped] = useState(false);
 
-    const [isFlipped, setIsFlipped] = useState(false);
+	const handleFlip = (e) => {
+		e.preventDefault();
+		setIsFlipped(!isFlipped);
+	};
 
-    const handleFlip = (e) => {
-        e.preventDefault();
-        setIsFlipped(!isFlipped);
-    };
+	return (
+		<div className='mt-3'>
+			<ReactCardFlip isFlipped={isFlipped} flipDirection='vertical' infinite>
+				<div className='card w-75 text-center'>
+					<div className='card-body'>
+						<h5 className='card-title'>Keyword</h5>
+						<p className='card-text'>{props.keyword}</p>
+						<button onClick={handleFlip} className='btn btn-primary'>
+							View definition
+						</button>
+					</div>
+				</div>
 
-    return(
-
-        <div className='mt-3'>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical" infinite>
-                <div className="card w-75 text-center">
-                    <div className="card-body">
-                        <h5 className="card-title">Keyword</h5>
-                        <p className="card-text">{props.keyword}</p>
-                        <button onClick={handleFlip} className="btn btn-primary">View definition</button>
-                    </div>
-                </div>
-        
-                <div className="card w-75 text-center">
-                    <div className="card-body">
-                        <h5 className='card-title'>Definition</h5>
-                        <p className="card-text">{props.definition}</p>
-                        <button onClick={handleFlip} className="btn btn-primary">View keyword</button>
-                    </div>
-                </div>
-            </ReactCardFlip>
-        </div>
-
-    )
+				<div className='card w-75 text-center'>
+					<div className='card-body'>
+						<h5 className='card-title'>Definition</h5>
+						<p className='card-text'>{props.definition}</p>
+						<button onClick={handleFlip} className='btn btn-primary'>
+							View keyword
+						</button>
+					</div>
+				</div>
+			</ReactCardFlip>
+		</div>
+	);
 }
 
 export default CardTester;
