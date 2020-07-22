dbd07d8588a9b7a9ecc08f711109173e6525a2a7
mwturner611 <mwturner611@gmail.com>
2020-07-21 19:40:33 -0500

    update edit cards button on review page

--- client/src/components/pages/Review.js
+++ client/src/components/pages/Review.js
@@ -73,8 +73,8 @@ const Review = (props) => {
     }, [check]);
 
     return(
-        <div>
-            <h1 className='text-center mt-5'>Review - {deckName}  <Button onClick={() => cardsPage(deckID,deckName)} className='text-center'>Edit Cards</Button></h1>
+        <div className='page text-center'>
+            <h1 className='text-center mt-5'>Review - {deckName}</h1><a onClick={() => cardsPage(deckID,deckName)} className='text-center'><span className='link'>Edit this deck</span></a>
             
             <div className='container mt-5'>
                 <div className='row'>
